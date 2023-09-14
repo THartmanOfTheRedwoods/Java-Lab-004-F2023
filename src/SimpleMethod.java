e3a94ac369ece60edc4be49ac534074044e5d134
FinnCl4rk <phinphan11@gmail.com>
2023-09-14 11:11:52 -0700

    in class work pt 2

--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -4,14 +4,14 @@ public class SimpleMethod {
     // Put your method code here
     public static int square(int nun) {
         return  nun * nun;
-
     }
 
 
     public static void main(String[] args) {
 
         // This is the method call, create the square method.
-        square(25);
+        int i  = square(25);
+        System.out.println(i);
     }
 
 }
--- src/SimpleMethod2.java
+++ src/SimpleMethod2.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class SimpleMethod2 {
     // Put your method code here, takes 1 integer, squares it, returns the integer result of the square.
-   // public static int square(int integer) {
-  //      return input * input;
-  //  }
+    public static int square(int input) {
+        return input * input;
+    }
 
 
     public static void main(String[] args) {
         // Put scanner code to get integer input here
         Scanner integer = new Scanner(System.in);
         int input = integer.nextInt();
-        System.out.println(input);
         // Modify the below call to assign the result of the method call to a variable.
-        //square(input);
+        int i = square(input);
         // Put the result System.out code here.
-        //System.out.println(input);
+        System.out.println(i);
     }
 }
