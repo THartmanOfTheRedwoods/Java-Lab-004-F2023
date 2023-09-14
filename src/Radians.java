3f312aaf3e8c9c58b8ad6d79e503e0ef4fd5ed6c
Ethan <ethanj955@gmail.com>
2023-09-14 12:35:05 -0700

    9/14/23

--- src/Radians.java
+++ src/Radians.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Radians {
 
-    public static <ADD TYPE> toRadians(<ADD TYPE> degrees) {
+    public static double toRadians(double degrees) {
         // Pur your code here
+        return degrees*Math.PI/180;
     }
 
-    public static <ADD TYPE> toDegrees(<ADD TYPE> radians) {
+    public static double toDegrees(double radians) {
         // Put your code here
+        return radians*Math.PI/180;
     }
 
     public static void main(String[] args) {
@@ -17,10 +19,12 @@ public class Radians {
         double degrees = Double.valueOf(scanner.nextLine());
 
         // Add System.out, your method call, and Math.toRadians call here
+        System.out.println(Math.toRadians(degrees));
 
         System.out.print("Type radians you'd like converted to degrees: ");
         double radians = Double.valueOf(scanner.nextLine());
 
         // Add System.out, your method call, and Math.toDegrees call here
+        System.out.println(Math.toDegrees(radians));
     }
 }
