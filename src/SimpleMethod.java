bb6bded73b020bd69b0a92c052093d291a72fefe
lance <fragasteven148@gmail.com>
2023-09-16 18:28:52 -0700

    Steven Fraga Submisson for LAB 4 CIS-12

--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -1,5 +1,10 @@
 public class SimpleMethod {
+    public static String square;
+
     // Put your method code here
+    public static int square(int num) {
+        return num * num;
+    }
 
     public static void main(String[] args) {
         // This is the method call, create the square method.
--- src/SimpleMethod2.java
+++ src/SimpleMethod2.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class SimpleMethod2 {
     // Put your method code here, takes 1 integer, squares it, returns the integer result of the square.
-
+    public static int square(int num) {
+        return num * num;
+    }
     public static void main(String[] args) {
         // Put scanner code to get integer input here
+        Scanner s = new Scanner(System.in);
+        System.out.println("input integer : ");
+        int input = Integer.parseInt(s.nextLine());
         // Modify the below call to assign the result of the method call to a variable.
-        square(input);
+        int answer = square(input);
         // Put the result System.out code here.
+        System.out.println(answer);
     }
 }
