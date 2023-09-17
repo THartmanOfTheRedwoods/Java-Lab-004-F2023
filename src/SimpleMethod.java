c4c1ef2a6e3b4c86debd8ff7e07355dd5192831c
Daniel Faulkner <dfaulkner707@gmail.com>
2023-09-16 21:55:48 -0700

    yippee

--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -1,6 +1,9 @@
 public class SimpleMethod {
     // Put your method code here
-
+public static void square(int yes) {
+    int bun = yes * yes;
+    System.out.println("The square of " + yes + "is: " + bun);
+}
     public static void main(String[] args) {
         // This is the method call, create the square method.
         square(25);
