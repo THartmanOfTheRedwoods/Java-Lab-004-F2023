5d43187aed2912c92b99a58a8b14251e7f773849
Kevin <kevinsicairos930@gmail.com>
2023-09-26 09:28:30 -0700

    finished lab 4

--- src/Radians.java
+++ src/Radians.java
@@ -2,25 +2,30 @@ import java.util.Scanner;
 
 public class Radians {
 
-    public static <ADD TYPE> toRadians(<ADD TYPE> degrees) {
+    public static double toRadians(double degrees, double radians) {
         // Pur your code here
+        return degrees = radians * 180 / Math.PI;
     }
 
-    public static <ADD TYPE> toDegrees(<ADD TYPE> radians) {
+    public static double toDegrees(double radians, double degrees) {
         // Put your code here
+        return radians = degrees * Math.PI / 180;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Type degrees you'd like converted to radians: ");
-        double degrees = Double.valueOf(scanner.nextLine());
+        double degrees = Double.parseDouble(scanner.nextLine());
 
         // Add System.out, your method call, and Math.toRadians call here
+        System.out.printf("%.5f\n",Math.toRadians(degrees));
 
         System.out.print("Type radians you'd like converted to degrees: ");
-        double radians = Double.valueOf(scanner.nextLine());
+        double radians = Double.parseDouble(scanner.nextLine());
 
         // Add System.out, your method call, and Math.toDegrees call here
+        System.out.println();
+        System.out.printf("%.5f\n",Math.toDegrees(radians));
     }
 }
