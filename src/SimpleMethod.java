bf2b37586cf75a754813bb4c1d5109f427e1b725
Cassandra Portlock <stellarluna30@gmail.com>
2023-10-04 12:55:22 -0700

    Completed Lab004

--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -1,6 +1,8 @@
 public class SimpleMethod {
     // Put your method code here
-
+    public static void square(int x) {
+        System.out.println(x * x);
+}
     public static void main(String[] args) {
         // This is the method call, create the square method.
         square(25);
--- src/SimpleMethod2.java
+++ src/SimpleMethod2.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class SimpleMethod2 {
     // Put your method code here, takes 1 integer, squares it, returns the integer result of the square.
+    public static int square(int x){
+        return x * x;
 
+    }
     public static void main(String[] args) {
         // Put scanner code to get integer input here
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number to have squared: ");
+        int input = Integer.parseInt(s.nextLine());
         // Modify the below call to assign the result of the method call to a variable.
-        square(input);
+        int result = square(input);
         // Put the result System.out code here.
+        System.out.printf("The square of %d is %d%n", input, result);
     }
 }
