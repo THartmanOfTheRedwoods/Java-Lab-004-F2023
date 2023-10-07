e250d399397b25b8b24883cca800cf842d0327c3
Joseph Verdin <josephverdin55@gmail.com>
2023-10-06 23:25:15 -0700

    Joseph Verdin completed Lab 004

--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -1,6 +1,9 @@
 public class SimpleMethod {
-    // Put your method code here
-
+    public static int square(int number) {
+        int squared = number * number;
+        System.out.println("Square of " + number +  " is: " + squared);
+        return squared;
+    }
     public static void main(String[] args) {
         // This is the method call, create the square method.
         square(25);
--- src/SimpleMethod3.java
+++ src/SimpleMethod3.java
@@ -8,14 +8,14 @@ public class SimpleMethod3 {
           Overloaded methods can even call each-other, but you'll have to type
           the arguments correctly and cast upon return.
      */
-    public static int square(int number) {
-        // Put your code here
-        return 0;
+    public static double square(int number) {
+        double intsq = Math.pow(number, 2);
+        return intsq;
     }
 
     public static double square(double number) {
-        // Put your code here
-        return 0;
+        double doubsq = Math.pow(number, 2);
+        return doubsq;
     }
 
     public static void main(String[] args) {
@@ -24,7 +24,7 @@ public class SimpleMethod3 {
         System.out.print("What number would you like to square: ");
         /* Integers Used */
         int input = Integer.valueOf(scanner.nextLine());
-        int result = square(input);
+        int result = (int) square(input);
         System.out.printf("The square of %d is %d%n", input, result);
 
         System.out.print("What number would you like to square: ");
