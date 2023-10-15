416725e3a6167de376d787c940cdbecd52480063
abracadabrakin <anakinprice69@gmail.com>
2023-10-14 20:16:04 -0700

    did a lil fixin up

--- src/SimpleMethod2.java
+++ src/SimpleMethod2.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 public class SimpleMethod2 {
     // Put your method code here, takes 1 integer, squares it, returns the integer result of the square.
-
+    public static int square(int number) {
+        int answer = number * number;
+        return answer;
+    }
     public static void main(String[] args) {
         // Put scanner code to get integer input here
+        System.out.println("enter number to square");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
         // Modify the below call to assign the result of the method call to a variable.
-        square(input);
+        int realanswer= square(number);
         // Put the result System.out code here.
+        System.out.println(number+"^2="+realanswer);
     }
 }
